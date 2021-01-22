30713efb0470eec0adb077e281fdf2c0209bae26
Mandula Naresh Babu <naresh2223@gmail.com>
2021-01-22 08:29:52 +0530

    22-Jan-2021 : Naresh : Reading excel data using jxl methods

--- src/com/io/demo/FileReaderTest.java
+++ src/com/io/demo/FileReaderTest.java
@@ -1,6 +1,5 @@
 package com.io.demo;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -9,14 +8,12 @@ public class FileReaderTest {
 	public static void main(String[] args) throws IOException {
 		
 		FileReader fr = new FileReader("./Files/Test Data.txt");
-		BufferedReader br = new BufferedReader(fr);
 	
 		int c;
 		
 		while ((c=fr.read())!=-1) {
 			System.out.println((char)c);
 		}
-		br.close();
 		fr.close();
 
 	}
