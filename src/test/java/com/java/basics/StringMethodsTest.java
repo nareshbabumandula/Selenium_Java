5eda9d85e1a2595a8d06c58ac805cecb307b747b
[nareshbabumandula] <naresh223@gmail.com>
2025-05-29 07:58:17 +0530

    Test Commit

--- src/test/java/com/java/basics/StringMethodsTest.java
+++ src/test/java/com/java/basics/StringMethodsTest.java
@@ -4,8 +4,15 @@ public class StringMethodsTest {
 
 	public static void main(String[] args) {
 		String str = "hello";
+		String sText = "world";
 		System.out.println(str.length());
 		System.out.println(str.toUpperCase());
+		System.out.println(str.charAt(0));
+		System.out.println(str.contains("he"));
+		System.out.println(str + " " + sText);
+		System.out.println(str.toUpperCase() + " " +sText.toUpperCase());
+		System.out.println(str.toUpperCase().concat(" ").concat(sText.toUpperCase()));
+		
 	}
 
 }
--- src/test/java/com/selenium/scripts/BrowserTest.java
+++ src/test/java/com/selenium/scripts/BrowserTest.java
@@ -11,6 +11,17 @@ public class BrowserTest {
 	public static void main(String[] args) throws InterruptedException {
 	  driver = new ChromeDriver();
 	  driver.get("https://www.mycontactform.com");
+	  
+	  String strBrowserTitle = driver.getTitle();
+	  System.out.println(strBrowserTitle);
+	  
+	  // Checkpoint or verification
+	  if(strBrowserTitle.contains("Free Contact and Email Forms")) {
+		  System.out.println("MyContactForm page title is correct");
+	  }else {
+		  System.out.println("MyContactForm page title is incorrect");
+	  }
+	  
 	  driver.manage().window().maximize();
 	  driver.findElement(By.id("user")).sendKeys("Manoja");
 	  Thread.sleep(1000);	  
