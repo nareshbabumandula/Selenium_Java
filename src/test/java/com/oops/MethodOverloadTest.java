94b1050e893fe3a35bec632891d6509f7215b115
[nareshbabumandula] <naresh223@gmail.com>
2025-06-10 08:59:30 +0530

    10-Jun-2025: Methodoverloading and Overriding

--- src/test/java/com/oops/MethodOverloadTest.java
+++ src/test/java/com/oops/MethodOverloadTest.java
@@ -2,9 +2,10 @@ package com.oops;
 
 public class MethodOverloadTest {
 	
-	// Instance variables or data members
+	 // Instance variables or data members
 	int a=10;
 	int b=20;
+	static int d=30;
 	
 	/*
 	 * Method overloading/compile-time polymorphism :
@@ -15,7 +16,7 @@ public class MethodOverloadTest {
 		int c=a+b;
 		System.out.println("Addition of integer a and b is :" +c);
 	}
-	
+
 	void addition(int a, int b, int c) {
 		int d=a+b+c;
 		System.out.println("Addition of integer a, b and c is :" +d);
@@ -38,13 +39,32 @@ public class MethodOverloadTest {
 	
 	// If there is no constructor defined inside a class then java compiler will create a default constructor
 
+	// static method and it belongs to a class
+	public static void subtraction() {
+		int a=20;
+		int b=30;
+		int c=a-b;
+		System.out.println("Subtraction of a and b is :" +c);
+	}
+	
+	public static void subtraction(int a, int b) {
+		int c=a-b;
+		System.out.println("Subtraction of a and b is :" +c);
+	}
+	
 	
 	public static void main(String[] args) {
 		MethodOverloadTest mot = new MethodOverloadTest();
-		mot.addition();
+		mot.addition(); // instance method call
 		mot.addition(100, 200, 300);
 		mot.addition(342.35435, 435.3453463);
-		
+		subtraction(); // static method call
+		System.out.println(d);
+		MethodOverloadTest.subtraction();
+		System.out.println(mot.a);
+		System.out.println(mot.b);
+		subtraction(70, 30);
+	
 		MethodOverloadTest mo = new MethodOverloadTest(50, 80);
 		
 	}
--- src/test/java/com/oops/MethodOverridingTest.java
+++ src/test/java/com/oops/MethodOverridingTest.java
@@ -7,19 +7,35 @@ package com.oops;
  */
 public class MethodOverridingTest extends MethodOverloadTest{
 	
+	int a=100;
+	int b=200;
+	
 	void addition() {
 		int c=a+b;
 		System.out.println("Addition of integer a and b in subclass is :" +c);
 	}
 	
+	public static void subtraction(int a, int b) {
+		int c=a-b;
+		System.out.println("Subtraction of a and b in subclass is :" +c);
+	}
+	
 	void m1() {
 		super.addition(); // call to super class addition method
+		this.addition();  // call to super class addition method
 	}
-			
+	
+	public MethodOverridingTest() {
+		super(); // call to a parent class constructor
+		System.out.println("No argument constructor..!");
+	}
+	
+	
 	public static void main(String[] args) {
 		MethodOverridingTest mot = new MethodOverridingTest();
 		mot.addition();
 		mot.m1();
+		subtraction(90, 20);
 	}
 
 }
