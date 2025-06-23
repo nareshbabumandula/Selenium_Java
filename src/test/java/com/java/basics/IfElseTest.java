a7f5a9a9a1ce09dc9fa258523f1a7d148c9f90d2
[nareshbabumandula] <naresh223@gmail.com>
2025-06-23 08:58:10 +0530

    23-Jun-2025: Control statements in Java

---
+++ src/test/java/com/java/basics/IfElseTest.java
@@ -0,0 +1,44 @@
+package com.java.basics;
+
+import java.util.Scanner;
+
+public class IfElseTest {
+	
+	public void compareNumbers() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of a: ");
+		int a = sc.nextInt();
+		System.out.println("Enter the value of b: ");
+		int b = sc.nextInt();
+		
+		if (a>b) {
+			// if block
+			System.out.println("a is greater than b");
+		} else if(a==b) {
+			// else if block
+			System.out.println("a is equal to b");
+		} else {
+			// else block
+			System.out.println("a is less than b");
+		}
+	}
+	
+	public void eveorOdd() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		int a = sc.nextInt();
+		
+		if (a%2==0) {
+			System.out.println(a + " is an even number");
+		} else {
+			System.out.println(a + " is an odd number");
+		}
+	}
+
+	public static void main(String[] args) {
+	   IfElseTest it = new IfElseTest();
+	   it.compareNumbers();
+	   it.eveorOdd();
+	}
+
+}
---
+++ src/test/java/com/java/loops/ControlStatements.java
@@ -0,0 +1,10 @@
+package com.java.loops;
+
+public class ControlStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
