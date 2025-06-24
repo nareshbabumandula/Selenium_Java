ef8aca4329e58d46e4ada275c1b56d92b31d4da6
[nareshbabumandula] <naresh223@gmail.com>
2025-06-24 08:46:39 +0530

    24-Jun-2025: Naresh: For Loops in Java

--- src/test/java/com/java/loops/ControlStatements.java
+++ src/test/java/com/java/loops/ControlStatements.java
@@ -2,9 +2,55 @@ package com.java.loops;
 
 public class ControlStatements {
 
+	void forLoopTest() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Iteration is: " + i);
+		}
+	}
+	
+	void forLoopReverseTest() {
+		System.out.println("For loop in reverse order..!");
+		for (int i = 10; i >= 0; i--) {
+			System.out.println("Iteration is: " + i);
+		}
+	}
+	
+	void forLoopEvenOddTest() {
+		for (int i = 0; i < 10; i++) {
+			if (i%2==0) {
+				System.out.println(i + " is an even number");
+			} else {
+				System.out.println(i + " is an odd number");
+			}
+		}
+	}
+	
+	void primeNumberTest(int num) {
+		int count=0;
+		for (int i = 2; i <= num/2; i++) {
+			if(num%i==0) {
+				count++;
+			}
+		}
+		if(num>2) {
+			if(count>=1) {
+				System.out.println(num + " is not a prime number");
+			}else {
+				System.out.println(num + " is a prime number");
+			}
+		} else {
+			System.out.println(num + " is not a prime number");
+		}
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ControlStatements cs = new ControlStatements();
+		cs.forLoopTest();
+		cs.forLoopReverseTest();
+		cs.forLoopEvenOddTest();
+		cs.primeNumberTest(2);
+		
 	}
 
 }
